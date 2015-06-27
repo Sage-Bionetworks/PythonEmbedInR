10d3e0931a7dbb2b49456dc5fff0a71328b48b99
Floooo <Florian_Schwendinger@gmx.at>
2015-06-27 15:47:39 +0200

    add options

---
+++ R/PyOptions.R
@@ -0,0 +1,12 @@
+pyOptions <-
+local({
+    options <- list(quote = TRUE, hash = TRUE, openbounds = "()")
+    function(option, value) {
+        if (missing(option)) return(options)
+        if (missing(value))
+            options[[option]]
+        else
+            options[[option]] <<- value
+    }
+})
+
