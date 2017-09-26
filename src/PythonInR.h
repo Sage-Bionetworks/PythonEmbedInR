c9d20653764e01d104e3b6acc77fad1306060ce6
bhoff <bruce.hoff@sagebase.org>
2017-09-26 11:08:45 -0700

    SYNR-1125

--- src/PythonInR.h
+++ src/PythonInR.h
@@ -11,9 +11,8 @@
 extern long pyrNamespaceCounter;
 extern int r_int_to_py_long_flag;
 
-#include <Python.h> // If you are a Linux user, did you realy install the Python headers? Try "sudo apt-get install python-dev" to fix this! :)
 #ifdef PYTHON_IN_R_NO_EXPLICIT_LINKING
-// include <Python.h> was moved above
+#include <Python.h> // If you are a Linux user, did you realy install the Python headers? Try "sudo apt-get install python-dev" to fix this! :)
 #else
 #define STRICT_R_HEADERS // To disable some R code else I get an compile error
 #endif
