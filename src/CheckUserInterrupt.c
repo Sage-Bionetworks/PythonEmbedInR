cf159d33a577568c9060465c98217909f659e0ab
bhoff <bruce.hoff@sagebase.org>
2017-09-27 13:46:01 -0700

    SYNR-1125

--- src/CheckUserInterrupt.c
+++ src/CheckUserInterrupt.c
@@ -1,14 +1,6 @@
 #include "CheckUserInterrupt.h"
 
-extern struct _Py_TrueStruct;
-extern struct _Py_FalseStruct;
-
-#ifndef Py_True
-#define Py_True ((PyObject *) &_Py_TrueStruct)
-#endif
-#ifndef Py_False
-#define Py_False ((PyObject *) &_Py_FalseStruct)
-#endif
+#include <boolobject.h>
 
 /*
  * Has R detected a keyboard interrupt?
@@ -17,10 +9,10 @@ PyObject *pythoninr_checkuserinterrupt(PyObject *self, PyObject *args) {
 	if (checkInterrupt()) {
 		Py_INCREF(Py_True);
 		return Py_True;
-	} else {
-		Py_INCREF(Py_False);
-		return Py_False;
 	}
+	Py_INCREF(Py_False);
+	return Py_False;
+
 }
 
 // from https://stat.ethz.ch/pipermail/r-devel/2011-April/060702.html
