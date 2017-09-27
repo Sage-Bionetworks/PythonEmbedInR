a586aacf1f098f596d87223d5f351fe55e0cbde0
bhoff <bruce.hoff@sagebase.org>
2017-09-27 15:00:30 -0700

    SYNR-1125

--- src/CheckUserInterrupt.c
+++ src/CheckUserInterrupt.c
@@ -3,11 +3,13 @@
 /*
  * Has R detected a keyboard interrupt?
  */
-void pythoninr_checkuserinterrupt(PyObject *self, PyObject *args) {
+PyObject *pythoninr_checkuserinterrupt(PyObject *self, PyObject *args) {
 	if (checkInterrupt()) {
-
+		Py_INCREF(Py_True);
+		return Py_True;
 	}
-
+	Py_INCREF(Py_False);
+	return Py_False;
 
 }
 
