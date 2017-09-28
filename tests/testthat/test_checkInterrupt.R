abfb8937369ca9801956497cfc91ab59828c3022
bhoff <bruce.hoff@sagebase.org>
2017-09-28 09:31:05 -0700

    SYNR-1125

--- tests/testthat/test_checkInterrupt.R
+++ tests/testthat/test_checkInterrupt.R
@@ -2,7 +2,8 @@ context("checkInterrupt")
 
 test_that("checkInterrupt", {
 	pyImport("ctypes")
-	pyExec("peirModule=ctypes.CDLL('PythonEmbedInR.so')")
+	# may have to use WinDLL() in place of CDLL() on Windows.  See: https://docs.python.org/3/library/ctypes.html
+	pyExec(sprintf("peirModule=ctypes.CDLL('PythonEmbedInR%s')", .Platform$dynlib.ext))
 	pyExec("result=peirModule.checkInterrupt()")
 	expect_equal(0, pyGet("result"))
 })
