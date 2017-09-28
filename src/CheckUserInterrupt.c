1bfa0f0695af05f4e67d2fdf57aa424a40b495b1
bhoff <bruce.hoff@sagebase.org>
2017-09-27 17:17:47 -0700

    SYNR-1125

--- src/CheckUserInterrupt.c
+++ src/CheckUserInterrupt.c
@@ -1,18 +1,5 @@
 #include "CheckUserInterrupt.h"
 
-/*
- * Has R detected a keyboard interrupt?
- */
-PyObject *pythoninr_checkuserinterrupt(PyObject *self, PyObject *args) {
-	if (checkInterrupt()) {
-		Py_INCREF(Py_True);
-		return Py_True;
-	}
-	Py_INCREF(Py_False);
-	return Py_False;
-
-}
-
 // from https://stat.ethz.ch/pipermail/r-devel/2011-April/060702.html
 void chkIntFn(void *dummy) {
   R_CheckUserInterrupt();
--- src/CheckUserInterrupt.h
+++ src/CheckUserInterrupt.h
@@ -2,30 +2,6 @@
 #include <signal.h>
 #include "PythonInR.h"
 
-#ifdef PYTHON_EXPLICIT_LINKING
-
-// copied from pyport.h
-#define PyAPI_DATA(RTYPE) extern __declspec(dllexport) RTYPE
-
-// copied from longinterpr.h
-struct _longobject {
-  PyObject_VAR_HEAD
-  unsigned ob_digit[1]; // modified from the original
-};
-
-
-// copied from boolobject.h
-/* Don't use these directly */
-PyAPI_DATA(struct _longobject) _Py_FalseStruct, _Py_TrueStruct;
-
-/* Use these macros */
-#define Py_False ((PyObject *) &_Py_FalseStruct)
-#define Py_True ((PyObject *) &_Py_TrueStruct)
-
-#endif
-
-PyObject *pythoninr_checkuserinterrupt(PyObject *self, PyObject *args);
-
 void chkIntFn(void *dummy);
 
 int checkInterrupt();
