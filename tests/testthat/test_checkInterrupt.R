6f413963bc3430047dcefae5265a82824911eee8
bhoff <bruce.hoff@sagebase.org>
2017-11-18 10:39:48 -0800

    SYNR-1142

--- tests/testthat/test_checkInterrupt.R
+++ tests/testthat/test_checkInterrupt.R
@@ -5,6 +5,8 @@ test_that("checkInterrupt", {
 	# may have to use WinDLL() in place of CDLL() on Windows.  See: https://docs.python.org/3/library/ctypes.html
 	sharedLibraryLocation<-system.file("libs", package="PythonEmbedInR")
 	sharedLibrary<-file.path(sharedLibraryLocation, sprintf("PythonEmbedInR%s", .Platform$dynlib.ext))
+	cat("will load library ", sharedLibrary, "\n")
+	cat("library exists: ", file.exists(sharedLibrary), "\n")
 	pyExec(sprintf("peirModule=ctypes.CDLL('%s')", sharedLibrary))
 	pyExec("result=peirModule.checkInterrupt()")
 	expect_equal(0, pyGet("result"))
