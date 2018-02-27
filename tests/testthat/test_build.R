311ea7711ca87617ad91d4ca6ade57404110e4e5
Kimyen <kimyen0816@gmail.com>
2018-02-27 13:47:44 -0800

    test tidy verse instead

--- tests/testthat/test_build.R
+++ tests/testthat/test_build.R
@@ -12,7 +12,7 @@ test_that("pip can be imported", {
 })
 
 test_that("other package can be installed", {
-  testPackage<-'ggplot2'
+  testPackage<-'tidyverse'
   try(remove.packages(testPackage), silent=T)
   install.packages(testPackage, repos='http://cran.fhcrc.org')
   library(testPackage, character.only=T)
