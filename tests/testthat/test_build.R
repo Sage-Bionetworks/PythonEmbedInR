c2d85f6513b3306d55062ff4e8ce6bd9d47a6a9b
Kimyen <kimyen0816@gmail.com>
2018-02-27 16:00:44 -0800

    Revert "test with cran instead of hutch mirror"
    This reverts commit 51ce7324b83e03ce6a858f66b27c78fdad5357bf.

--- tests/testthat/test_build.R
+++ tests/testthat/test_build.R
@@ -12,8 +12,8 @@ test_that("pip can be imported", {
 })
 
 test_that("other package can be installed", {
-  testPackage<-'ggplot2'
+  testPackage<-'tidyverse'
   try(remove.packages(testPackage), silent=T)
-  install.packages(testPackage, repos='https://cran.r-project.org/')
+  install.packages(testPackage, repos='http://cran.fhcrc.org')
   library(testPackage, character.only=T)
 })
