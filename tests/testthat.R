14d6b1d06bdd3ae44d284310cbee0c242a385870
Kimyen <kimyen0816@gmail.com>
2018-03-23 16:55:41 -0700

    debug

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,10 +6,12 @@ package_dir <- gsub("(PythonEmbedInR).*", "\\1", getwd())
 testthat_dir <- file.path(package_dir, "tests", "testthat")
 
 pyImport("sys")
+pyExecp("sys.path")
 # clean up sys.path to ensure that testthat does not use user's installed packages
 pyExec(sprintf("sys.path = [x for x in sys.path if x.startswith(\"%s\") or \"PythonEmbedInR\" in x]", package_dir))
 # add testthat to python search path
 pyExec(sprintf("sys.path.insert(0, \"%s\")", testthat_dir))
+pyExecp("sys.path")
 
 test_check("PythonEmbedInR")
 
