3b6287f816a9647635f2eff6e88e4602a623320f
Kimyen <kimyen0816@gmail.com>
2018-03-23 12:21:05 -0700

    change how to add testthat folder to python search path

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,17 @@
 library(testthat)
 library(PythonEmbedInR)
 
+# determine the testthat path
+package_dir <- gsub("(PythonEmbedInR).*", "\\1", getwd())
+testthat_dir <- file.path(package_dir, "tests", "testthat")
+
+# add testthat to python search path
+pyImport("sys")
+pyExec(sprintf("sys.path.append(\"%s\")", testthat_dir))
+
 test_check("PythonEmbedInR")
 
 # uninstall pandas
 pyExit()
 print("Uninstalling pandas ...")
-package_dir <- gsub("(PythonEmbedInR).*", "\\1", getwd())
 pyExec(sprintf("install_pandas.main('uninstall', '%s')", package_dir))
