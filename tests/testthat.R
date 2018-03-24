58cdfae5b63393e9739bcba7e0dd25cfaf014906
Kimyen <kimyen0816@gmail.com>
2018-03-23 17:09:33 -0700

    use current dir

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,14 +3,11 @@ library(PythonEmbedInR)
 
 # determine the testthat path
 package_dir <- gsub("(PythonEmbedInR).*", "\\1", getwd())
-testthat_dir <- file.path(package_dir, "tests", "testthat")
 
 pyImport("sys")
 pyExecp("sys.path")
-# clean up sys.path to ensure that testthat does not use user's installed packages
-pyExec(sprintf("sys.path = [x for x in sys.path if x.startswith(\"%s\") or \"PythonEmbedInR\" in x]", package_dir))
-# add testthat to python search path
-pyExec(sprintf("sys.path.insert(0, \"%s\")", testthat_dir))
+# insert current dir to python search path
+pyExec("sys.path.insert(0, \".\")")
 pyExecp("sys.path")
 
 test_check("PythonEmbedInR")
