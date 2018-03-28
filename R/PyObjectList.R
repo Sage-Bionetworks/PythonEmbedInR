c8520d7d600a69274ec80f2c0911f6be9e4b4c97
Kimyen <kimyen0816@gmail.com>
2018-03-28 12:38:18 -0700

    Revert "revert"
    This reverts commit 5522072046480de75091de454db3fa58d3f43905.

--- R/PyObjectList.R
+++ R/PyObjectList.R
@@ -7,6 +7,7 @@
 
 PythonInR_List <-
     R6Class("PythonInR_List",
+            lock_objects = FALSE,
             portable = TRUE,
             inherit = PythonInR_Object,
             public = list(
@@ -49,6 +50,7 @@ PythonInR_List <-
 
 PythonInR_ListNoFinalizer <-
     R6Class("PythonInR_List",
+            lock_objects = FALSE,
             portable = TRUE,
             inherit = PythonInR_List,
             public = list(
