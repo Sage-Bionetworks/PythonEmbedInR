f36a74149d6991a0e8e3c57715a17162edc6b054
KimYen Ladia <kimyen0816@gmail.com>
2019-03-11 17:41:47 -0700

    include rlang

--- jenkins.sh
+++ jenkins.sh
@@ -8,7 +8,7 @@ mkdir -p ../RLIB
 
 ## Install required R libraries
 echo "try(remove.packages('synapser'), silent=T);" > installReqPkgs.R
-echo "list.of.packages <- c('pack', 'R6', 'testthat', 'rjson');" >> installReqPkgs.R
+echo "list.of.packages <- c('pack', 'R6', 'testthat', 'rjson', 'rlang');" >> installReqPkgs.R
 echo "if(length(list.of.packages)) install.packages(list.of.packages, repos='http://cran.fhcrc.org')" >> installReqPkgs.R
 R --vanilla < installReqPkgs.R
 rm installReqPkgs.R
